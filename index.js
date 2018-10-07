3eee20b056d712fc515a1597d7db4af481d34b88
poyu <poyuki9@gmail.com>
2018-10-07 13:15:45 +0300

    *perf: - *add more content to index.html and more style things. add my slider demonstration*

--- index.js
+++ index.js
@@ -9,3 +9,27 @@ function openDetails(event) {
         el.classList.add('fa-plus');
     }
 }
+
+function controlSlider(direction, event) {
+    let controlButton=$(event.currentTarget),
+        opositeDirection=direction==='right'?'left':'right',
+        opositeControlButton=$(`svg.${opositeDirection}`),
+        slider=$('.slider-content'),
+        leftPos=parseInt(slider.css('left')),
+        rightPos=parseInt(slider.css('right')),
+        toMove=225;
+    if (!slider.is(':animated')) {
+        switch (direction) {
+            case 'right':
+                if (rightPos + toMove === 0) controlButton.hide();
+                opositeControlButton.show();
+                slider.animate({'left': `${leftPos - toMove}px`}, 500);
+                break;
+            case 'left':
+                if (leftPos + toMove === 0) controlButton.hide();
+                opositeControlButton.show();
+                slider.animate({'left': `${leftPos + toMove}px`}, 500);
+                break;
+        }
+    }
+}
